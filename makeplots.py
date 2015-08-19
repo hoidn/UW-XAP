997f049ffdb37844ea9071f62143d60e5b86bf59
Oliver Hoidn <ohoidn@psana1611.pcdsn>
2015-08-19 15:39:16 -0700

    forgot a file

---
+++ makeplots.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+get_ipython().magic(u'run src/data_access.py')
+get_ipython().magic(u'run src/XES_converter.py V ScV_40um_0.09 2 ScV_2um_1.00 ScV_5um_1.00 ScV_10um_1.00 ScV_20um_1.00 ScV_40um_1.00 ScV_60um_1.00 -b ScV_40um_1.00')
+get_ipython().magic(u'run src/XES_converter.py Sc ScV_40um_0.09 1 ScV_2um_1.00 ScV_5um_1.00 ScV_10um_1.00 ScV_20um_1.00 ScV_40um_1.00 ScV_60um_1.00 -b ScV_40um_1.00')
+get_ipython().magic(u'run src/XES_converter.py Ti TiCr_30um_0.11 1 TiCr_2um_1.00 TiCr_5um_1.00 TiCr_15um_1.00 TiCr_30um_1.00')
+get_ipython().magic(u'run src/XES_converter.py Cr TiCr_30um_0.11 2 TiCr_2um_1.00 TiCr_5um_1.00 TiCr_15um_1.00 TiCr_30um_1.00')
