d3651fd1c11f53ec8b9f4ce636def72c74cd53dc
Oliver Hoidn <ohoidn@psana1502.pcdsn>
2015-10-09 19:53:21 -0700

    Bug fixes

--- xes/scripts/xes_spectra.py
+++ xes/scripts/xes_spectra.py
@@ -6,7 +6,7 @@ if __name__ =='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('datapaths', nargs = '+', help = 'Paths of CSPAD data files to process into spectra and plot.')
     parser.add_argument('--pxwidth', '-p', type = int, default = 3, help = 'Pixel width of CSPAD subregion to sum.')
-    parser.add_argument('--calibration', '-c', type = str, help = 'Path of data to use for calibration of the energy scale (if --kalpha_kbeta_calibration is selected but a calibration file is not provided) and identification of the subregion of the CSPAD to process into a spectrum. If not provided this parameter defaults to the first path in datapaths.')
+    parser.add_argument('--calibration', '-c', type = str, help = 'Path of data to use for calibration of the energy scale (if --kalpha_kbeta_calibration is selected but a calibration file is not provided). If not provided this parameter defaults to the first path in datapaths.')
     parser.add_argument('--subtraction', '-d', type = str, help = 'Path of a data file to use as a dark frame subtraction')
     parser.add_argument('--kalpha_kbeta_calibration', '-k', action = 'store_true', help = 'Enable automaic generation of energy calibration based on k alpha and k beta peak locations if --calibration_load_path is not given.')
     parser.add_argument('--eltname', '-e', default = '', help = 'Element name. This parameter is required for generating an energy scale using --kalpha_kbeta_calibration.')
