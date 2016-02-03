22b0a15d7729a0f3ca041fd9549583bb23b97cd4
Oliver Hoidn <ohoidn@gmail.com>
2016-02-03 15:04:18 -0800

    more cleanup

--- dataccess/build/lib/dataccess/__init__.py
+++
@@ -1,6 +0,0 @@
-#import os
-
-#_ROOT = os.path.abspath(os.path.dirname(__file__))
-#def abs_path(path):
-#    return os.path.join(_ROOT, 'data', path)
-
--- dataccess/build/lib/dataccess/config_syspath.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('/reg/neh/home/ohoidn/anaconda/lib/python2.7/site-packages')
-sys.path.append('/reg/neh/home/ohoidn/anaconda/lib/python2.7/site-packages/pathos-0.2a1.dev0-py2.7.egg')
-sys.path.append('/reg/neh/home/ohoidn/anaconda/lib/python2.7/site-packages/dataccess-1.0-py2.7.egg')
-# The version of multiprocessing installed on the psana system is incompatible
-# with pathogen. We need to install multiprocessing locally and push its
-# to sys.path
-#sys.path.insert(0, '/reg/neh/home/ohoidn/anaconda/lib/python2.7/site-packages/multiprocessing/')
