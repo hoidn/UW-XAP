f506c5cafee18f89a370f137f5b6561b4170c8ad
Oliver Hoidn <ohoidn@gmail.com>
2016-05-18 16:10:10 -0700

    Update venv

--- mecana_venv/bin/get_objgraph.py
+++ mecana_venv/bin/get_objgraph.py
@@ -1,4 +1,7 @@
-#!/reg/neh/home2/ohoidn/venv_test/mecana2/bin/python
+#!/usr/bin/env python2.7
+
+import os; activate_this=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'activate_this.py'); exec(compile(open(activate_this).read(), activate_this, 'exec'), dict(__file__=activate_this)); del os, activate_this
+
 # EASY-INSTALL-SCRIPT: 'dill==0.2.5','get_objgraph.py'
 __requires__ = 'dill==0.2.5'
 __import__('pkg_resources').run_script('dill==0.2.5', 'get_objgraph.py')
--- mecana_venv/bin/pox_launcher.py
+++ mecana_venv/bin/pox_launcher.py
@@ -1,4 +1,7 @@
-#!/reg/neh/home2/ohoidn/venv_test/mecana2/bin/python
+#!/usr/bin/env python2.7
+
+import os; activate_this=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'activate_this.py'); exec(compile(open(activate_this).read(), activate_this, 'exec'), dict(__file__=activate_this)); del os, activate_this
+
 # EASY-INSTALL-SCRIPT: 'pox==0.2.2','pox_launcher.py'
 __requires__ = 'pox==0.2.2'
 __import__('pkg_resources').run_script('pox==0.2.2', 'pox_launcher.py')
